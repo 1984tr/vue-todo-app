89b9b9f38e3fd424bcf2f49e1a5dd591f196e46b
tris84 <jollytris@gmail.com>
2020-08-13 00:22:01 +0900

    npm i -D babel-loader css-loader vue-style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,32 @@
-const path = require('path')
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
-    // 진입점 : 이 프로젝트에서 가장 먼저 실행되는 파일
-    entry: {
-        app: path.join(__dirname, 'main.js') // 별칭 app
-    },
-    // 결과물에 대한 설정
-    output: {
-        filename: '[name].js', // entry의 별칭이 설정됨
-        path: path.join(__dirname, 'dist')
-    },
-    module: {
-
-    },
-    plugins: []
-}
+  // 진입점 : 이 프로젝트에서 가장 먼저 실행되는 파일
+  entry: {
+    app: path.join(__dirname, "main.js"), // 별칭 app
+  },
+  // 결과물에 대한 설정
+  output: {
+    filename: "[name].js", // entry의 별칭이 설정됨
+    path: path.join(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+};
