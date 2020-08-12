b1e4b2b92f1d9228a3e1f0baa91775c93cf3752e
tris84 <jollytris@gmail.com>
2020-08-12 23:28:41 +0900

    webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+    // 진입점 : 이 프로젝트에서 가장 먼저 실행되는 파일
+    entry: {
+        app: path.join(__dirname, 'main.js') // 별칭 app
+    },
+    // 결과물에 대한 설정
+    output: {
+        filename: '[name].js', // entry의 별칭이 설정됨
+        path: path.join(__dirname, 'dist')
+    },
+    module: '',
+    plugins: ''
+}
