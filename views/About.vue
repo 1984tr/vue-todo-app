4382ee4c050c64eefb4d67ed0ab3a826a90fb913
tris84 <jollytris@gmail.com>
2020-08-23 23:38:59 +0900

    router

---
+++ views/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="home">
+        <div>
+            <h2>
+                Hello, Vue!
+            </h2>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
